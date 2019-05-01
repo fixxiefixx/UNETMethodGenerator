c313318edd7e9eb604951a2122887042c56a8192
fixx <fixx@hotmail.de>
2019-05-01 17:42:12 +0200

    First commit

---
+++ UNETMethodGenerator/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace UNETMethodGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_methodHead = new System.Windows.Forms.TextBox();
+            this.textBox_output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Method head:";
+            // 
+            // textBox_methodHead
+            // 
+            this.textBox_methodHead.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_methodHead.Location = new System.Drawing.Point(93, 6);
+            this.textBox_methodHead.Name = "textBox_methodHead";
+            this.textBox_methodHead.Size = new System.Drawing.Size(344, 20);
+            this.textBox_methodHead.TabIndex = 1;
+            this.textBox_methodHead.Text = "private void DoSomething(bool b1, float f1)";
+            this.textBox_methodHead.TextChanged += new System.EventHandler(this.textBox_methodHead_TextChanged);
+            // 
+            // textBox_output
+            // 
+            this.textBox_output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_output.Location = new System.Drawing.Point(12, 43);
+            this.textBox_output.Multiline = true;
+            this.textBox_output.Name = "textBox_output";
+            this.textBox_output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_output.Size = new System.Drawing.Size(425, 256);
+            this.textBox_output.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 311);
+            this.Controls.Add(this.textBox_output);
+            this.Controls.Add(this.textBox_methodHead);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "UNETMethodGenerator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_methodHead;
+        private System.Windows.Forms.TextBox textBox_output;
+    }
+}
+
---
+++ UNETMethodGenerator/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UNETMethodGenerator
+{
+    public partial class Form1 : Form
+    {
+
+        private class UArg
+        {
+            public string typeName;
+            public string argName;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            MakeItHappen();
+        }
+
+        private void MakeItHappen()
+        {
+            string methodHead = textBox_methodHead.Text;
+            int klammerStartIndex = methodHead.IndexOf('(');
+            int klammerEndIndex = methodHead.IndexOf(')');
+            if (klammerStartIndex > 0 && klammerEndIndex > klammerStartIndex)
+            {
+                string preKlammerText = methodHead.Substring(0, klammerStartIndex);
+                string[] preKlammerWords = preKlammerText.Split(" ".ToCharArray());
+                if(preKlammerWords.Length>1)
+                {
+                    string methodName = preKlammerWords[preKlammerWords.Length - 1];
+                    string methodModifier = "";
+                    for(int i=0;i<preKlammerWords.Length-1;i++)
+                    {
+                        methodModifier += preKlammerWords[i];
+                        if (i < preKlammerWords.Length - 2)
+                            methodModifier += " ";
+                    }
+
+                    string betweenBracketsText = methodHead.Substring(klammerStartIndex + 1, klammerEndIndex - klammerStartIndex - 1);
+                    string[] argstexts = betweenBracketsText.Split(",".ToCharArray());
+                    List<UArg> args = new List<UArg>();
+                    
+                    foreach (string argtext in argstexts)
+                    {
+                        string[] argsWords = argtext.Split(" ".ToCharArray());
+                        if(argsWords.Length>=2)
+                        {
+                            UArg arg = new UArg();
+                            arg.typeName = argsWords[argsWords.Length - 2];
+                            arg.argName = argsWords[argsWords.Length - 1];
+                            args.Add(arg);
+                        }
+                    }
+                    string argsCallNames = "";
+                    for(int i=0;i<args.Count; i++)
+                    {
+                        argsCallNames += args[i].argName;
+                        if (i < args.Count - 1)
+                            argsCallNames += ",";
+                    }
+
+                    //Jetzt werden die Methoden generiert
+
+                    StringBuilder sb = new StringBuilder();
+
+                    //Network Method
+                    sb.Append(methodModifier);
+                    sb.Append(' ');
+                    sb.Append(methodName);
+                    sb.Append("Network(");
+                    sb.Append(betweenBracketsText);
+                    sb.Append(")\r\n{\r\n");
+                    sb.Append("if (isLocalPlayer)\r\n");
+                    sb.Append("Cmd").Append(methodName).Append('(').Append(argsCallNames).Append(");\r\n");
+                    sb.Append("else\r\n{\r\nif(isServer)\r\n");
+                    sb.Append("Rpc").Append(methodName).Append('(').Append(argsCallNames).Append(");\r\n}\r\n");
+                    sb.Append("Final").Append(methodName).Append('(').Append(argsCallNames).Append(");\r\n}\r\n\r\n");
+
+                    //Command Method
+                    sb.Append("[Command]\r\n");
+                    sb.Append("private void ");
+                    sb.Append("Cmd").Append(methodName).Append('(').Append(betweenBracketsText);
+                    sb.Append(")\r\n{\r\n");
+                    sb.Append("if (!isLocalPlayer)\r\n");
+                    sb.Append("Final").Append(methodName).Append('(').Append(argsCallNames).Append(");\r\n");
+                    sb.Append("Rpc").Append(methodName).Append('(').Append(argsCallNames).Append(");\r\n}\r\n\r\n");
+
+                    //Rpc Method
+                    sb.Append("[ClientRpc]\r\n");
+                    sb.Append("private void ");
+                    sb.Append("Rpc").Append(methodName).Append('(').Append(betweenBracketsText);
+                    sb.Append(")\r\n{\r\n");
+                    sb.Append("if (isServer || isLocalPlayer)\r\nreturn;\r\n");
+                    sb.Append("Final").Append(methodName).Append('(').Append(argsCallNames).Append(");\r\n}\r\n\r\n");
+
+                    //Final Method
+                    sb.Append("private void ");
+                    sb.Append("Final").Append(methodName).Append('(').Append(betweenBracketsText);
+                    sb.Append(")\r\n{\r\n");
+                    sb.Append("//Your code here\r\n");
+                    sb.Append("}\r\n");
+
+                    textBox_output.Text = sb.ToString();
+                }
+            }
+
+        }
+
+        private void textBox_methodHead_TextChanged(object sender, EventArgs e)
+        {
+            MakeItHappen();
+        }
+
+        
+
+
+    }
+}
